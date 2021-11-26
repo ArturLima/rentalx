2dbe9e2e4cdad809054025c5bcf6b15dfe4bf7ca
Artur <arturpeixotoo@gmail.com>
2021-11-26 20:51:48 -0300

    incldue integration test with superTest framework

---
+++ src/modules/cars/useCases/listCategory/ListCategoriesController.spec.ts
@@ -0,0 +1,55 @@
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { hash } from "bcryptjs";
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = await uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "admin", created_at, driver_license)
+      values('${id}','Artur3','arturpeixoto@gmail.com', '${password}', true, 'now()', 'XXXXXX')
+      `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to list all categories", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "arturpeixoto@gmail.com",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app).get("/categories");
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0].name).toEqual("Category Supertest");
+  });
+});
